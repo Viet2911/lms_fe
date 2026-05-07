09c82d594f182f2113f5957f7887f2ffb3c0a6d7
Viet2911 <130074365+Viet2911@users.noreply.github.com>
2026-05-07 17:48:48 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -3,7 +3,7 @@
 
 window.LMS_CONFIG = {
   // Development
-  API_BASE: 'https://football-techno-medicines-candy.trycloudflare.com/api',
+  API_BASE: 'https://london-fountain-obtained-yard.trycloudflare.com/api',
 
   // Production - Railway
   // API_BASE: 'https://lms-be-9uuc.onrender.com/api',
